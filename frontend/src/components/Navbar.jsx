ac3b65dbc1c632484695e814b389185a33634775
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2025-03-31 04:27:08 +0500

    working on dropdown menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { href, Link } from 'react-router-dom';
 import { HiMiniBars3CenterLeft } from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
 import { HiOutlineUser } from "react-icons/hi2";
 import { MdOutlineFavoriteBorder } from "react-icons/md";
 import { HiOutlineShoppingCart } from "react-icons/hi";
+import avatarImage from '../assets/avatar.png';
+
+const Navigation = [
+  { name: 'Dashboard', href: '/dashboard' },
+  { name: 'Orders', href: '/order' },
+  { name: 'Cart Page', href: '/cart' },
+  { name: 'Checkout', href: '/checkout' },
+]
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
+
+  const currentUser = 1; 
   return (
     <header className='max-w-screen-lg mx-auto px-4 py-6'>
-      <nav className='flex justify-between items-center'>
+      <nav className='flex justify-between items-center md:gap-3 gap-3'>
         <div className='flex items-center md:gap-14 gap-4'>
           <Link to="/">
             <HiMiniBars3CenterLeft className='size-6' />
@@ -21,13 +32,39 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className='flex items-center md:gap-2 gap-2'>
-         <HiOutlineUser className='size-6'/>
+        <div className='relative flex items-center md:gap-2 gap-2'>
+          <div>
+            {
+              currentUser? <>
+              <button onClick={()=> setIsDropdownOpen(!isDropdownOpen) } className='relative'>
+                <img src={avatarImage} alt="" className={`size-7 rounded-full ${currentUser? 'ring-2 ring-blue-500' : ''}`} />
+              </button>
+              {
+                isDropdownOpen && (
+                  <div>
+                    <ul>
+                      {
+                        Navigation.map((item)=>(
+                          <li key={item.name} >
+                            <Link to={item.href}>
+                              {item.name}
+                            </Link>                            
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  </div>
+                )
+              }
+              </> : <Link to='/login'> <HiOutlineUser className='size-6'/> </Link> 
+            }
+          </div>
+        
          <button className='hidden sm:block'><MdOutlineFavoriteBorder className='size-6'/></button>
          
          <Link to='/cart' className='bg-primary rounded-md sm:px-6 py-1 flex items-center gap-1'>
             <HiOutlineShoppingCart className='size-6'/>
-            <span>0</span>
+            <span className='text-sm font-semibold sm:ml-1'>0</span>
          </Link>
         </div>
       </nav>
