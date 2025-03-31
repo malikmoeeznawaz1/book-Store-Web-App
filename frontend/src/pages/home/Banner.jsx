dbf688a7659c61f0a9566c804eeb2423f1e0b4d4
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2025-03-31 05:15:14 +0500

    Hero section is done

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -3,7 +3,11 @@ import bannerImg from '../../assets/banner.png'
 
 const Banner = () => {
   return (
-    <div className='flex flex-col md:flex-row py-2 justify-between items-center gap-10'>
+    <div className='flex flex-col md:flex-row-reverse py-2 justify-between items-center gap-10'>
+      <div className='md:w-1/2 w-full'>
+        <img src={bannerImg} alt="Banner"  />
+      </div>
+
       <div className='md:w-1/2 w-full '>
         <h1 className='md:text-3xl text-2xl font-medium mb-7'>New Releases This Week</h1>
         <p className='mb-10'>
@@ -11,11 +15,6 @@ const Banner = () => {
         </p>
 
         <button className='btn-primary'>Subscribe</button>
-      </div>
-      
-      
-      <div className='md:w-1/2 w-full'>
-        <img src={bannerImg} alt="Banner"  />
       </div>
     </div>
   )
