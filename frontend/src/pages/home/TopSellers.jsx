13cb30473e8bbacecba94f784e6e75257bdaef93
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2025-04-01 19:40:12 +0500

    filtered books modified

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -4,6 +4,7 @@ const categories = ["Choose a genre", "Fiction", "Horror", "Adventure"]
 
 const TopSellers = () => {
   const [books, setBooks] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(["Choose a genre"]);
 
   useEffect(()=>{
     fetch("books.json")
@@ -11,13 +12,18 @@ const TopSellers = () => {
       .then((data) => setBooks(data))
   },[])
 
+  const filteredBooks = selectedCategory == "Choose a genre"? books: books.filter((book) => book.category === selectedCategory.toLowerCase());
+  console.log(filteredBooks);
+  
+
   return (
     <div className='py-4'>
       <h2 className='text-xl font-semibold mb-3'> Top Sellers</h2>
       
       {/* category filter */}
       <div>
-        <select name="category" id="category" className=' bg-gray-100 p-2 text-sm focus:outline-none border border-gray-100 rounded-md'>
+        <select onChange={(e) => setSelectedCategory(e.target.value)} value={selectedCategory}
+         name="category" id="category" className=' bg-gray-100 p-2 text-sm focus:outline-none border border-gray-100 rounded-md'>
           {categories.map((category, index) => (
             <option key={index} value={category} >{category}</option>
           ))}
