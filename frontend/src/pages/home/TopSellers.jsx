d67ab515a42055746a6a9cc58db946a521449cc4
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2025-04-01 18:38:23 +0500

    books json is collected and stored in public folder

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TopSellers = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(()=>{
+    fetch("books.json")
+      .then(res => res.json())
+      .then((data) => setBooks(data))
+  },[])
+
   return (
     <div>Top Sellers</div>
   )
